d9235ef59ece9d340dd11a8f83b7bbbc9921fb26
Денис <denislegrace@gmail.com>
2023-03-26 16:47:09 +0500

    Рефакторинг, хинты, докстринги

--- engine.py
+++ engine.py
@@ -1,11 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 # engine = create_engine(
 #     "postgresql+psycopg2://postgres:postgres@127.0.0.1:5432/pythonmind",
 # )
-engine = create_engine('sqlite:///db/database.db')
+engine = create_engine("sqlite:///db/database.db")
 Session = sessionmaker(bind=engine)
 session = Session()
 
--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 import configparser
 
 config = configparser.ConfigParser()
-config.read('.env')
+config.read(".env")
 
 general_env = config["General"]
 
-API_TOKEN = general_env['API_TOKEN']
+API_TOKEN = general_env["API_TOKEN"]
